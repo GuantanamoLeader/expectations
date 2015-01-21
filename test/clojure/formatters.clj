9dd069c5278e50b304f2a8a6fd05506f253266fe
Евгений <elizavetaskull@gmail.com>
2015-01-21 13:59:21 +0600

    fix test

--- test/clojure/formatters.clj
+++ test/clojure/formatters.clj
@@ -1,21 +1,22 @@
 (ns formatters
   (:use expectations)
-  (:require [expectations.formatters.html-formatter :refer 
-             [->HTMLFormatter instance render html-key]]
-            [expectations.formatters.formatter :refer [class->key] :as fmt]
+  (:require [expectations.formatters.html :refer 
+             [->HTMLFormatter render html-key instance]]
+            [expectations.formatters.formatter :as fmt]
             [hiccup.core :refer [html]]
             [clojure.java.io :refer [writer file]]))
 
 (defrecord HTMLFmtrM [output names-of-columns])
-(def new-key (class->key HTMLFmtrM))
+
+(def new-key ::my-html)
 
 (defmethod fmt/passed new-key [this name meta]
-  (render (:output this)
-          (html 
-            [:tr.passed 
-             [:td name]
-             [:td "Pass"] 
-             [:td (or (:info meta) "")]])))
+  (->> (html 
+        [:tr.passed 
+         [:td name]
+         [:td "Pass"] 
+         [:td (or (:info meta) "nooobies")]])
+       (render (:output this))))
 
 (def col-names ["test-name" "status" "info"])
 
@@ -23,11 +24,10 @@
   "set html render for notifer"
   {:expectations-options :before-run}
   []
-(derive new-key html-key)
+  (derive new-key html-key)
   (swap! *formatter* 
          (fn [_] 
-           ;instance
-           (->HTMLFmtrM (writer (file "test.html")) col-names)
+           (vary-meta (->HTMLFmtrM (writer (file "test.html")) col-names) assoc :type new-key)
            )))
 
 (expect 1 1)
